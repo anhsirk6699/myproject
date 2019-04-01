b57a738ee70f346bb1463c40fe5853ec90c886ff
“anhsirk6699” <anhsirk6699@gmail.com>
2019-04-01 10:54:28 +0530

    ‘completed1’

--- hello.c
+++ hello.c
@@ -3,5 +3,8 @@ int main()
 {
    // printf() displays the string inside quotation
    printf("Hello, World!");
+	printf("Hello, World!1");
+	printf("Hello, World!2");
+	printf("Hello, World!3");
    return 0;
 }
